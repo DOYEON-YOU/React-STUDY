262f0452433af35b0d047b78c497304ec257fd38
doyeonyou <doyeonyou@naver.com>
2022-05-17 06:10:47 +0900

    learn react & made shop project

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+/* eslint-disable */
 import './App.css';
+import { Button, Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
+import { useState } from 'react';
+import data from './data'
 
 function App() {
+
+  let [shoes] = useState()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar bg='dark' variant='dark'>
+        <Container>
+          <Navbar.Brand href='#home'>YEON</Navbar.Brand>
+          <Nav className='me-auto'>
+            <Nav.Link href='#home'>Home</Nav.Link>
+            <Nav.Link href='#features'>Features</Nav.Link>
+            <Nav.Link href='#pricing'>Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className='main-bg'></div>
+      <Container className='grid'>
+        <Row>
+          <Col sm>
+            <img
+              src='https://codingapple1.github.io/shop/shoes2.jpg'
+              alt='shoes'
+            />
+            <h4>상품명</h4>
+            <p>상품 설명</p>
+          </Col>
+          <Col sm>
+            <img
+              src='https://codingapple1.github.io/shop/shoes1.jpg'
+              alt='shoes'
+            />
+            <h4>상품명</h4>
+            <p>상품 설명</p>
+          </Col>
+          <Col sm>
+            <img
+              src='https://codingapple1.github.io/shop/shoes3.jpg'
+              alt='shoes'
+            />
+            <h4>상품명</h4>
+            <p>상품 설명</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
